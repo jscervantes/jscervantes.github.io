9321e50681d75134cf783a5ff6054d1cdc70cfaa
jscervantes <jscervantes00@gmail.com>
2024-11-25 01:50:23 -0500

    Move resume to public folder

--- jscervantes-portfolio/src/components/Hero.tsx
+++ jscervantes-portfolio/src/components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero: React.FC = () => {
       <div id="name" className="big-box four columns">
         <h1>Jose Cervantes</h1>
       </div>
-      <HeroButton label="Resume" href="../assets/new_jscervantes_resume.pdf" />
+      <HeroButton label="Resume" href="/new_jscervantes_resume.pdf" />
       <HeroButton label="Github" href="https://github.com/jscervantes" />
       <HeroButton label="Linkedin" href="https://www.linkedin.com/in/josecervantesnyc/" />
       <HeroButton label="Music" href="https://soundcloud.com/s-vhs" />
